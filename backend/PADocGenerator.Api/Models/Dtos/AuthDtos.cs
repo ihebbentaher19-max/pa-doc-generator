2ad55bb169484ad095463155c378a758c0534683
Contributeur <contributeur@projet.local>
2026-07-31 08:45:40 +0100

    role admin backend

--- backend/PADocGenerator.Api/Models/Dtos/AuthDtos.cs
+++ backend/PADocGenerator.Api/Models/Dtos/AuthDtos.cs
@@ -1,5 +1,5 @@
 namespace PADocGenerator.Api.Models.Dtos;
 
 public record LoginRequestDto(string Email, string Password);
-public record LoginResponseDto(string Token, DateTime ExpiresAtUtc, string FullName, string Role);
+public record LoginResponseDto(Guid Id, string Token, DateTime ExpiresAtUtc, string FullName, string Role);
 public record RegisterUserDto(string FullName, string Email, string Password);
